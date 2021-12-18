1b48cbd52a5f0d7e55504eee3806bca87107dbac
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-12-18 09:31:55 +0530

    Updated

---
+++ src/main/java/com/realtimechat/WebChatAppApplication.java
@@ -0,0 +1,14 @@
+package com.realtimechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatAppApplication.class, args);
+	}
+
+}
+	
---
+++ src/main/java/com/realtimechat/controllers/MessageController.java
@@ -0,0 +1,19 @@
+package com.realtimechat.controllers;
+
+import com.realtimechat.models.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message) {
+        return message;
+    }
+
+}
